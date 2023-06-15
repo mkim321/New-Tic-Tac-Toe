7ae522c67c6e0305527d85d9441e340a98af7fe6
Misun <mkim321@my.bcit.ca>
2023-06-15 12:29:33 -0700

    main

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import {useState} from 'react';
+
+function Square({value, onSquareClick}) {
+  const [value, setValue] = useState(null);
+
+  function handleClick() {
+    setValue('X');
+  }
+  return (
+    <button 
+      className="square"
+      onClick={onSquareClick}
+    >
+      {value}
+    </button>
+  );
+}
+
+export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  return (
+    <>
+      <div className="board-row">
+        <Square value={squares[1]}/>
+        <Square value={squares[2]}/>
+        <Square value={squares[3]}/>
+      </div>
+      <div className="board-row">
+        <Square value={squares[4]}/>
+        <Square value={squares[5]}/>
+        <Square value={squares[6]}/>
+      </div>
+      <div className="board-row">
+        <Square value={squares[7]}/>
+        <Square value={squares[8]}/>
+        <Square value={squares[9]}/>
+      </div>
+    </>
+  );
+}
